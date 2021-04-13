c59e7577f82f2e8a93b539ee10e7023e80256f2d
jfillion12394 <jfillion12394@gmail.com>
2021-04-13 22:54:10 +0200

    correction

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,13 +21,7 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/index.html.twig').$this->showProducts();
+        return $this->twig->render('Home/home.html.twig');
     }
 
-    public function showProducts()
-    {
-        $products = ['guitare', 'bass', 'bonjo', 'cithare', 'lyre'];
-
-        return $this->twig->render('Home/products.html.twig',  ['products' =>  $products]);
-    }
 }
